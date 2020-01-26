5f3a2748d29f01b56b547d2f00b17e69f28c1a58
Mike Olivieri <mike@olivieris.com>
2020-01-25 23:49:37 -0500

    fix for CORS issue

--- routes/index.js
+++ routes/index.js
@@ -1,15 +1,12 @@
 var express = require('express');
-var cors=require ("cors");
 var router = express.Router();
+var cors = require('cors');
 const db = require('./queries')
-var app = express();
-
-app.use(cors());
 
 // Get all branches
 router.get('/branches', function (req, res) {
-	console.warn("in /branches");
 	db.getBranches().then(function (data) {
+		res.setHeader('Access-Control-Allow-Origin', '*');
 		res.send(data);
 	}).catch(function (ew) {
 		res.send(ew);
@@ -18,30 +15,29 @@ router.get('/branches', function (req, res) {
 
 // Get branch of a specific zipcode
 router.get('/branch/:zipcode', function (req, res) {
-	console.warn("in /branch with zipcode");;
 	db.searchBranch(req.params.zipcode).then(function (data) {
+		res.setHeader('Access-Control-Allow-Origin', '*');
 		res.send(data);
 	}).catch(function (ew) {
 		res.send(ew);
 	});
 });
 
+
+router.options('/branch', function (req, res) {
+    cors();
+});
+
 // Edit branch
 router.put('/branch', function (req, res) {
-	console.warn("in /branch MAO");
-	console.warn(req.body);
+    cors();
+	console.log(req.body);
 	db.editBranch(req.body).then(function (data) {
+		res.setHeader('Access-Control-Allow-Origin', '*');
 		res.send(data);
 	}).catch(function (ew) {
 		res.send(ew);
 	});
-});
-router.options('/branch', function(req, res) {
-	console.warn ("in OPTIOns");
-	res.setHeader("Access-Control-Allow-Origin", "*");
-	res.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	res.setHeader("Access-Control-Allow-Methods", "GET, PUT, POST, PATCH, DELETE, OPTIONS");
-	res.setHeader("Access-Control-Request-Method", "*");
 });
 
 module.exports = router;
